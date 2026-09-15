eb17564c739353ae0c98cca66f41738abf108754
roundeeesss <yaroslavgoncharov31@gmail.com>
2026-09-15 13:48:30 +0200

    add global Book model

--- Bookworm/Models/Book.swift
+++ Bookworm/Models/Book.swift
@@ -1,4 +1,19 @@
 import Foundation
 import SwiftData
 
+@Model
+class Book {
+    var title: String
+    var author: String
+    var genre: Genre
+    var review: String
+    var rating: Int
 
+    init(title: String, author: String, genre: Genre, review: String, rating: Int) {
+        self.title = title
+        self.author = author
+        self.genre = genre
+        self.review = review
+        self.rating = rating
+    }
+}
