637a6ffb507f44a86e8f73bbcd263ad7a87f7cfa
roundeeesss <yaroslavgoncharov31@gmail.com>
2026-09-15 17:09:02 +0200

    add isBookValid computed property to validate title and author name's fields and add date property to track the date when book was added

--- Bookworm/Models/Book.swift
+++ Bookworm/Models/Book.swift
@@ -8,12 +8,18 @@ class Book {
     var genre: Genre
     var review: String
     var rating: Int
+    var date: Date
 
-    init(title: String, author: String, genre: Genre, review: String, rating: Int) {
+    init(title: String, author: String, genre: Genre, review: String, rating: Int, date: Date) {
         self.title = title
         self.author = author
         self.genre = genre
         self.review = review
         self.rating = rating
+        self.date = date
+    }
+    var isBookValid: Bool {
+        [title, author]
+            .allSatisfy { !$0.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty }
     }
 }
