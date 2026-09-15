27fec4ce39dea87e2f394eb3ab910b183cf61e92
roundeeesss <yaroslavgoncharov31@gmail.com>
2026-09-15 14:04:46 +0200

    add SwiftData model container

--- Bookworm/MyApp.swift
+++ Bookworm/MyApp.swift
@@ -1,9 +1,11 @@
 import SwiftUI
+import SwiftData
 
 @main struct MyApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: Book.self)
     }
 }
