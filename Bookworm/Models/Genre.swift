c75377c9e6395d171c06d051cd989e6a376cb55a
roundeeesss <yaroslavgoncharov31@gmail.com>
2026-09-15 13:46:38 +0200

    add enum with book genres

---
+++ Bookworm/Models/Genre.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+enum Genre: String, CaseIterable, Codable {
+    case fantasy = "Fantasy"
+    case horror = "Horror"
+    case kids = "Kids"
+    case mystery = "Mystery"
+    case poetry = "Poetry"
+    case romance = "Romance"
+    case biography = "Biography"
+}
